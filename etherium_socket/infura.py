e8062df84093ba3911f37e9eabe8011beb6ee44b
MihaiBorsu <mihai.borsu@gmail.com>
2021-04-26 20:01:10 +0300

    am inceput in sfartit licenta

---
+++ etherium_socket/infura.py
@@ -0,0 +1,7 @@
+#https://medium.com/validitylabs/how-to-interact-with-the-ethereum-blockchain-and-create-a-database-with-python-and-sql-3dcbd579b3c0
+#https://www.devteam.space/blog/how-to-use-blockchain-to-build-a-scalable-database/
+
+
+from web3 import Web3
+
+web3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/3a5a9ce6db6a4c4daf0454d130711f75"))
